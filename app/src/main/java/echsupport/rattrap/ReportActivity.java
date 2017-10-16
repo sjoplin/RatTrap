85194fe525b8971ff6e8a883b53dde7c4d80ec9a
emileesisson <emilee.sisson@gmail.com>
2017-10-15 20:31:46 -0400

    Created Report java class and screen for reporting rat sightings

---
+++ app/src/main/java/echsupport/rattrap/ReportActivity.java
@@ -0,0 +1,16 @@
+package echsupport.rattrap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Emilee on 10/15/17.
+ */
+
+public class ReportActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}
